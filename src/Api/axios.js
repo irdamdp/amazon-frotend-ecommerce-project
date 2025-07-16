648496ebf0aa047f2e3500ba66d246f9ea09923d
irdamdp <mirdadapollo@gmail.com>
2025-07-16 18:34:39 +0300

    frotend of the amazon ecommerce project for deployment

--- src/Api/axios.js
+++ src/Api/axios.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 const axiosinstance = axios.create({
-  baseURL: "http://127.0.0.1:5001/clone-132f6/us-central1/api", // The API URL
+  // baseURL: "http://127.0.0.1:5001/clone-132f6/us-central1/api", // The API URL
+  baseURL: "https://amazon-api-deploy-l4fk.onrender.com", // deployed version of render.com
 });
 
 export { axiosinstance };
